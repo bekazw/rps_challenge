42ccb12be914cff92a7f329a85370741f84338a3
bekazw <bekazw@gmail.com>
2018-05-16 14:12:40 +0300

    bs + redux

--- src/components/App/LoginForm/LoginIn.js
+++ src/components/App/LoginForm/LoginIn.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './LoginForm.css';
+
 
 class ComLogIn extends Component {
     constructor (props) {
@@ -11,23 +13,54 @@ class ComLogIn extends Component {
 
     render() {
         return (
-            <div className = "LogInForm">
-                <input 
-                    type = "email"
-                    className = "LogInLogin"
-                />
-                <input 
-                    type = "password"
-                    className = "LogInPass"
-                />
+            <div className="LoginForm">      
+                      
+                <div class="form-group">                
+                    <label for="LogInLogin">
+                       Email address
+                    </label>                    
+                    <input 
+                        type="email" 
+                        class="form-control LogInInput" 
+                        id="LogInLogin"
+                        aria-describedby="emailHelp" 
+                        required 
+                        name = "LogInLogin"
+                        placeholder="Enter email"                                   
+                    />
+                    
+                </div>
+                <div class="form-group">
+                    <label 
+                        for="LogInPass">
+                        Password
+                    </label>
+                    <input 
+                        type="password" 
+                        class="form-control LogInInput" 
+                        id="LogInPass" 
+                        placeholder="Password"
+                        required
+                        name = "LogInPass" 
+                    />
+                </div>               
                 <button 
-                    className = "LogInBut"
-                >
-                LogIn
-                </button>    
+                    type="submit" 
+                    class="btn btn-primary"
+                    id = "LogBut">
+                    LogIn
+                </button>  
+                <button 
+                    type="submit" 
+                    class="btn btn-warning" 
+                    id = "RegBut">
+                    Registration
+                </button>               
             </div>
         )
     }
 }
 
 export default ComLogIn;
+
+//<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
