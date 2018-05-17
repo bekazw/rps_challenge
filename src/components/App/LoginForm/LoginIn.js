a6b0b73333a8a5ed9c9daf2e15f2a8cac4b2a26a
bekazw <bekazw@gmail.com>
2018-05-17 10:19:42 +0300

    login in check

--- src/components/App/LoginForm/LoginIn.js
+++ src/components/App/LoginForm/LoginIn.js
@@ -11,48 +11,78 @@ class ComLogIn extends Component {
         }
     }
 
+    valueUpdate = (e, who) => {
+        if (who === "l") {
+            this.setState({
+                curLogin : e.target.value
+            })
+        }
+        else {
+            this.setState({
+                curPass : e.target.value
+            })
+        }
+    }
+    tryToLogIn = () =>{
+        fetch("http://localhost:1000/auth/"+this.state.curLogin+"/"+this.state.curPass+"")   
+        .then(function(response) {
+          return response.json()
+        }).then(function(json) {
+            console.dir(json);
+          if (json !== 404) {  
+             console.log("Welcome")          
+            }
+        }).catch(function(ex) {
+          console.log('parsing failed', ex)
+        })   
+    }
+
     render() {
         return (
             <div className="LoginForm">      
                       
-                <div class="form-group">                
-                    <label for="LogInLogin">
+                <div className="form-group">                
+                    <label htmlFor="LogInLogin">
                        Email address
                     </label>                    
                     <input 
                         type="email" 
-                        class="form-control LogInInput" 
+                        className="form-control LogInInput" 
                         id="LogInLogin"
                         aria-describedby="emailHelp" 
                         required 
                         name = "LogInLogin"
-                        placeholder="Enter email"                                   
+                        placeholder="Enter email"      
+                        onChange = {e=>this.valueUpdate(e,"l")}                             
                     />
                     
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <label 
-                        for="LogInPass">
+                        htmlFor="LogInPass">
                         Password
                     </label>
                     <input 
                         type="password" 
-                        class="form-control LogInInput" 
+                        className="form-control LogInInput" 
                         id="LogInPass" 
                         placeholder="Password"
                         required
                         name = "LogInPass" 
+                        onChange = {e=>this.valueUpdate(e,"p")}
                     />
                 </div>               
                 <button 
                     type="submit" 
-                    class="btn btn-primary"
-                    id = "LogBut">
+                    className="btn btn-primary"
+                    id = "LogBut"
+                    onClick = {this.tryToLogIn}
+                    >
                     LogIn
                 </button>  
                 <button 
                     type="submit" 
-                    class="btn btn-warning" 
+                    className="btn btn-warning" 
                     id = "RegBut">
                     Registration
                 </button>               
