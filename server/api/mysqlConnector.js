a01a33d1ab89905cf7d3b76dee6ec25f12e7a2ca
bekazw <bekazw@gmail.com>
2018-05-17 17:41:38 +0300

    vue xample

--- server/api/mysqlConnector.js
+++ server/api/mysqlConnector.js
@@ -1,15 +1,28 @@
 let mysql = require('mysql');
+
+let x = "none";
 let connection = mysql.createConnection({
     host : 'localhost',
-    user : '',
-    password : ''
+    user : 'root',
+    password : '',
+    database: "test"
 });
-
+    
 connection.connect();
 
-connection.query('SELECT * AS sol', (err, rows, fields) => {
-    if (err) throw err;
-    console.log(row[0].sol)
+connection.query('SELECT pass AS sol FROM authData WHERE login LIKE "admin"', (err, rows, fields) => {
+    x = rows[0].sol;    
+    console.log(x);     
 });
+connection.end(); 
 
-connection.end();
+
+
+
+
+
+    
+
+
+
+
