b986bc86aefeaf1f7f8ae5b3a2e92dd3cb91b132
bekazw <bekazw@gmail.com>
2018-05-22 13:47:54 +0300

    api 0_1

---
+++ vue_client/api_1/src/components/AuthPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id = "AuthForm">
+    <form>
+        <div class="form-group">
+            <label for="AuthPageLogin">Email address</label>
+            <input type="email" class="form-control" id="AuthPageLogin" aria-describedby="emailHelp" placeholder="Enter email">           
+        </div>
+        <div class="form-group">
+            <label for="AuthPagePass">Password</label>
+            <input type="password" class="form-control" id="AuthPagePass" placeholder="Password">
+        </div>       
+        <button type="submit" class="btn btn-primary">LogMe</button>
+        <button type="submit" class="btn btn-warning">RegMe</button>
+    </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'AuthPage',
+        props : {
+
+        }
+    }
+</script>
+
+<style lang = "scss">  
+    #AuthForm {
+        position: absolute;
+        width: 30%;
+        left : 60%;
+    }
+    #AuthPageLogin, #AuthPagePass {
+        text-align: center;
+    }
+</style>
